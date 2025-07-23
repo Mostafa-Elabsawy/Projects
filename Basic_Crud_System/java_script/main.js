58a5d1c1bbba97a8a5b034758fbc208c028561a3
Mostafa-Elabsawy <mostfaelabsawy@gmail.com>
2025-07-23 11:04:44 +0300

    update_crud_system

--- Basic_Crud_System/java_script/main.js
+++ Basic_Crud_System/java_script/main.js
@@ -2,8 +2,10 @@ var formName = document.getElementById("Name");
 var formPrice = document.getElementById("Price");
 var formCategory = document.getElementById("Category");
 var formDescription = document.getElementById("Description");
+var formSearch = document.getElementById("Search");
 var addBTN = document.getElementById("button_add");
 var updateBTN = document.getElementById("button_update");
+var searchBTN = document.getElementById("button_search");
 var All_products = [];
 var key = "DB";
 var updating_index = -1;
@@ -18,53 +20,83 @@ function StartUp() {
     labels[i].classList.add("fs-5");
     labels[i].classList.add("fw-medium");
   }
-  
-  
 }
 function Add_Element() {
   let NewElement = Retrieve_DataInputs();
   All_products.push(NewElement);
-  console.log("New Element Added",NewElement);
+  console.log("New Element Added", NewElement);
 }
-function Delete_Element(index)
-{
+function Delete_Element(index) {
   let deleted_ele = All_products[index];
   All_products.splice(index, 1);
   Update_LocalStorge(key, All_products);
   display();
-  console.log("Element Deleted",deleted_ele);
+  console.log("Element Deleted", deleted_ele);
 }
 function Update_Element(index) {
   updateBTN.style.display = "block";
   addBTN.style.display = "none";
-  Inputs = document.getElementsByClassName("form-control");
+  let Inputs = document.getElementsByClassName("form-control");
   updating_index = index;
   let values = [];
   for (let i in All_products[index]) values.push(All_products[index][i]);
   for (let i = 0; i < values.length; i++) Inputs[i].value = values[i];
   console.log("Updating Element");
 }
+function search(Keyword) {
+  Keyword = Keyword.trim().split(" ");
+  let box = ``;
+  for (let i = 0; i < All_products.length; i++) {
+    if (check_keyword(All_products[i], Keyword)) {
+      box += `
+      <tr>
+      <td>${All_products[i].name || "---"}</td>
+      <td>${All_products[i].price == "" ? "---" : All_products[i].price + "$"}</td>
+      <td>${All_products[i].category || "---"}</td>
+      <td>${All_products[i].desc || "---"}</td>
+      <td><button class="bg-danger btn " onclick="Delete_Element(${i})">Delete</button></td>
+      <td><button class="bg-warning btn" onclick="Update_Element(${i})">Update</button></td> 
+      </tr>`;
+    }
+    document.getElementById("table_body").innerHTML = box;
+    console.log("ok");
+  }
+}
+function check_keyword(product, Keyword) {
+  let flag = true;
+  Keyword.forEach((ele) => {
+    ele = ele.toLowerCase();
+    if (
+      !(
+        product.name.toLowerCase().includes(ele) ||
+        product.category.toLowerCase().includes(ele) ||
+        product.desc.toLowerCase().includes(ele)
+      )
+    )
+      flag = false;
+  });
+  return flag;
+}
 function clear_inputs() {
   let arr = document.getElementsByClassName("form-control");
   for (let i = 0; i < arr.length; i++) arr[i].value = null;
-  console.log("input cleared"); 
+  console.log("input cleared");
 }
-function display() {
+function display(content = All_products) {
   var box = "";
-  for (var i = 0; i < All_products.length; i++)
-  { 
+  for (var i = 0; i < content.length; i++) {
     box += `
             <tr>
-            <td>${All_products[i].name || "---"}</td>
-            <td>${(All_products[i].price=="") ? "---" :(All_products[i].price+'$')}</td>
-            <td>${All_products[i].category ||"---"}</td>
-            <td>${All_products[i].desc||"---"}</td>
+            <td>${content[i].name || "---"}</td>
+            <td>${content[i].price == "" ? "---" : content[i].price + "$"}</td>
+            <td>${content[i].category || "---"}</td>
+            <td>${content[i].desc || "---"}</td>
             <td><button class="bg-danger btn " onclick="Delete_Element(${i})">Delete</button></td>
             <td><button class="bg-warning btn" onclick="Update_Element(${i})">Update</button></td> 
             </tr>`;
   }
   document.getElementById("table_body").innerHTML = box;
-  console.log("table displayed",All_products);
+  console.log("table displayed", All_products);
 }
 function Access_data_in_LocalStorge(key) {
   let Answer = null;
@@ -80,12 +112,12 @@ function Update_LocalStorge(key, value) {
 }
 function Retrieve_DataInputs() {
   let Element = {
-    name: formName.value ,
-    price:formPrice.value ,
-    category: formCategory.value ,
-    desc: formDescription.value ,
+    name: formName.value,
+    price: formPrice.value,
+    category: formCategory.value,
+    desc: formDescription.value,
   };
-  console.log("Element retrived:",Element);
+  console.log("Element retrived:", Element);
   return Element;
 }
 addBTN.addEventListener("click", () => {
@@ -101,5 +133,9 @@ updateBTN.addEventListener("click", function () {
   addBTN.style.display = "block";
   Update_LocalStorge(key, All_products);
   display();
+});
+formSearch.addEventListener("keyup", () => {
+  console.log("retrived the search word = ", formSearch.value);
+  search(formSearch.value);
 });
 StartUp();
